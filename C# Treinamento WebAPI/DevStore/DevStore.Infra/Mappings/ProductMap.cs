45be3b2fb4edce3f99f9779ce85dabe9e506ef28
Josiel Alves <josiel.alves@smn.domain>
2018-10-22 16:39:33 -0300

    WebAPI C#

---
+++ C# Treinamento WebAPI/DevStore/DevStore.Infra/Mappings/ProductMap.cs
@@ -0,0 +1,28 @@
+﻿using DevStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+
+namespace DevStore.Infra.Mappings
+{
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable("Product");
+            HasKey(x => x.Id);
+            Property(x => x.Title).HasMaxLength(160).IsRequired();
+            Property(x => x.Price).IsRequired();
+            Property(x => x.AcquireDate).IsRequired();
+
+            HasRequired(x => x.Category);
+            //Opcional
+            //HasOptional
+            //N para N
+            //HasMany
+            
+        }
+    }
+}
